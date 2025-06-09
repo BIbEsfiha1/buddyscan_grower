a6cd335305c1e8359d6786b09a1dee6a45a12a39
Caramaschi <156044967+BIbEsfiha1@users.noreply.github.com>
2025-06-09 17:22:24 -0300

    Move theme toggle to hamburger menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,6 @@ import { useTranslation } from 'react-i18next';
 import { useAuth } from '../contexts/AuthContext';
 import PlusIcon from './icons/PlusIcon';
 import ProfileDropdown from './ProfileDropdown';
-import ThemeToggle from './ThemeToggle';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
@@ -66,7 +65,6 @@ const Header: React.FC<HeaderProps> = ({
           <IconButton onClick={onOpenAddModal} color="inherit" aria-label={t('header.add_plant')}>
             <PlusIcon className="w-5 h-5" />
           </IconButton>
-          <ThemeToggle />
           <IconButton color="inherit" className="relative">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
               <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
